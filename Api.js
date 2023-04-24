da3dd387583198a0d0939a79c2adf911dfcadf1a
Fakhruddin <43902192+fbhabrana@users.noreply.github.com>
2023-04-24 15:16:30 +0530

    Add files via upload
    Weather App created using reactjs

---
+++ Api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a80bead4d6msh58430ba0f206783p1c63f1jsncf57e7ae3b9c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "452d886134998a6c8e0d63d3359219ca";
